d144c5de0ea47b6bd699382769bd7b689cefa8b0
Vinicius Ortiz <vinicius-ortiz@outlook.com>
2023-05-08 22:26:33 -0300

    Ajuste service bus

---
+++ Fiap.Microservices.Ui/Extensions/ServiceBusStoreCatalog.cs
@@ -0,0 +1,36 @@
+﻿using Azure.Messaging.ServiceBus;
+using Common.ServiceBus;
+
+namespace Fiap.Microservices.Ui.Extensions
+{
+    public static class ServiceBusStoreCatalog
+    {
+        public async static Task<IServiceCollection> AddServiceBusStoreCatalog(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration["ServiceBusConnection"];
+            var queueName = "ingredients";
+
+            var consumer = new Consumer(connectionString, queueName);
+
+            await consumer.Start(MessageHandler, ErrorHandler);
+
+            return services;
+        }
+
+        async static Task MessageHandler(ProcessMessageEventArgs args)
+        {
+            string body = args.Message.Body.ToString();
+            Console.WriteLine($"Received: {body}");
+
+            // complete the message. message is deleted from the queue. 
+            await args.CompleteMessageAsync(args.Message);
+        }
+
+        // handle any errors when receiving messages
+        static Task ErrorHandler(ProcessErrorEventArgs args)
+        {
+            Console.WriteLine(args.Exception.ToString());
+            return Task.CompletedTask;
+        }
+    }
+}
--- Fiap.Microservices.Ui/Program.cs
+++ Fiap.Microservices.Ui/Program.cs
@@ -9,6 +9,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContextService();
+await builder.Services.AddServiceBusStoreCatalog(builder.Configuration);
 
 var app = builder.Build();
 
